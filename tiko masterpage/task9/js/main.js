dd8faacafaf915f8495ffbc44d99b118a6d86527
Noah Bauer <147219@msa.asd20.org>
2019-02-20 13:12:00 -0700

    oofen

---
+++ tiko masterpage/task9/js/main.js
@@ -0,0 +1,19 @@
+function replaceText() {
+    var noun1 = document.getElementById("noun1").value;
+    document.getElementById("Bnoun1").innerHTML = noun1;
+
+    var noun2 = document.getElementById("noun1").value;
+    document.getElementById("Bnoun2").innerHTML = noun2;
+
+    var pluralnoun1 = document.getElementById("pluarlnoun1").value;
+    document.getElementById("Bpluralnoun1").innerHTML = pluralnoun1;
+
+    var pluralnoun2 = document.getElementById("pluralnoun2").value;
+    document.getElementById("Bpluralnoun2").innerHTML = pluralnoun2;
+
+    var verb1 = document.getElementById("verb1").value;
+    document.getElementById("Bverb1").innerHTML = verb1;
+
+    var adj1 = document.getElementById("adj").value;
+    document.getElementById("Badj1").innerHTML = adj1;
+}
