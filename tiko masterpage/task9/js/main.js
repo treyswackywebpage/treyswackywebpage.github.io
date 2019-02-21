a6aaaade87519812ac31090bb18b247e254af7ab
Noah Bauer <147219@msa.asd20.org>
2019-02-21 12:55:49 -0700

    oofen

--- tiko masterpage/task9/js/main.js
+++ tiko masterpage/task9/js/main.js
@@ -1,19 +1,31 @@
 function replaceText() {
     var noun1 = document.getElementById("noun1").value;
-    document.getElementById("Bnoun1").innerHTML = noun1;
+    document.getElementById("Bnoun9").innerHTML = noun1;
 
-    var noun2 = document.getElementById("noun1").value;
-    document.getElementById("Bnoun2").innerHTML = noun2;
-
-    var pluralnoun1 = document.getElementById("pluarlnoun1").value;
+    var pluralnoun1 = document.getElementById("pluralnoun1").value;
     document.getElementById("Bpluralnoun1").innerHTML = pluralnoun1;
 
-    var pluralnoun2 = document.getElementById("pluralnoun2").value;
-    document.getElementById("Bpluralnoun2").innerHTML = pluralnoun2;
+    var noun3 = document.getElementById("noun3").value;
+    document.getElementById("Bnoun3").innerHTML = noun3;
 
     var verb1 = document.getElementById("verb1").value;
     document.getElementById("Bverb1").innerHTML = verb1;
 
-    var adj1 = document.getElementById("adj").value;
+    var adj1 = document.getElementById("adj1").value;
     document.getElementById("Badj1").innerHTML = adj1;
+
+    var adj2 = document.getElementById("adj2").value;
+    document.getElementById("Badj2").innerHTML = adj2;
+
+    var adj3 = document.getElementById("adj3").value;
+    document.getElementById("Badj3").innerHTML = adj3;
+
+    var noun2 = document.getElementById("noun2").value;
+    document.getElementById("Bnoun2").innerHTML = noun2;
+    
+    var verb2 = document.getElementById("verb2").value;
+    document.getElementById("Bverb2").innerHTML = verb2;
+
+    var infverb1 = document.getElementById("infverb1").value;
+    document.getElementById("Binfverb1").innerHTML = infverb1;
 }
