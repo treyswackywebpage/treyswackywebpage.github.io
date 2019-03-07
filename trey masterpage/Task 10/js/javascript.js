98ba9500b9cd11ba9b587e43918acdeb00faf5c4
Trey DeaBueno <146798@msa.asd20.org>
2019-03-07 13:07:09 -0700

    fixed main page

---
+++ trey masterpage/Task 10/js/javascript.js
@@ -0,0 +1,13 @@
+/*
+Trey DeaBueno
+Task 10
+March 6, 2019
+*/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
