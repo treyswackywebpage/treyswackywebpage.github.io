ae9b6cb6e01684e6b5d383f8403a7f48902c6765
Noah Bauer <147219@msa.asd20.org>
2019-03-12 13:10:19 -0600

    oofen

--- tiko masterpage/task10/js/main.js
+++ tiko masterpage/task10/js/main.js
@@ -1,3 +1,15 @@
-revealPassword () {
+function revealPassword () {
     document.getElementById("password-box").hidden = false;
+}
+function sendEmail() {
+    var password = document.getElementById("password").value;
+    var realPassword = "Dank Dr.Wilson";
+    if (password == realPassword) {
+        document.getElementById("email-sent").hidden = false;
+        document.getElementById("password-needed").hidden = true;
+    }
+    else
+    {
+        document.getElementById("password-needed").hidden = false;
+    }
 }
