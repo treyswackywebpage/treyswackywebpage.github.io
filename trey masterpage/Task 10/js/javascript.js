81927c34ee1668957b70d5c691ce1fb05895b653
Trey DeaBueno <146798@msa.asd20.org>
2019-03-12 13:04:47 -0600

    task 10 navbar

--- trey masterpage/Task 10/js/javascript.js
+++ trey masterpage/Task 10/js/javascript.js
@@ -3,11 +3,37 @@ Trey DeaBueno
 Task 10
 March 6, 2019
 */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+$(window).resize(function() {
+    var targetWidth = 580;
+    if ($(window).width() >= targetWidth) {
+        scrollFunction();
+    } else {
+        document.getElementById("active").style.height = "37px";
+    }
+});
+
+window.onscroll = function() {
+    var targetWidth = 580;
+    if ($(window).width() >= targetWidth) {
+        scrollFunction();
+    }
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("active").style.fontSize = "17px";
+        document.getElementById("active").style.height = "55px";
+        document.getElementById("navbar").style.padding = "10px 10px";
+        document.getElementById("logo").style.fontSize = "25px";
+        document.getElementById("logoimg").style.width = "25px";
+        document.getElementById("logoimg").style.height = "25px";
+    } else {
+        document.getElementById("active").style.fontSize = "20px";
+        document.getElementById("active").style.height = "59px";
+        document.getElementById("navbar").style.padding = "15px 10px";
+        document.getElementById("logo").style.fontSize = "35px";
+        document.getElementById("logoimg").style.width = "35px";
+        document.getElementById("logoimg").style.height = "35px";
+    }
 }
