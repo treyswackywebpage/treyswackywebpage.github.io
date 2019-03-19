55e45941fbb6553594b5fd321c500a2dd1a6f73c
Noah Bauer <147219@msa.asd20.org>
2019-03-19 13:10:08 -0600

    oofen

--- tiko masterpage/task10/js/main.js
+++ tiko masterpage/task10/js/main.js
@@ -2,16 +2,27 @@ function revealPassword () {
     document.getElementById("password-box").hidden = false;
 }
 function sendEmail() {
-    var templateParams = {
-        name: document.getElementById("email").value,
-        notes: document.getElementById("content").value
-    }
     var password = document.getElementById("password").value;
     var realPassword = "Dank Dr.Wilson";
     if (password == realPassword) {
         document.getElementById("email-sent").hidden = false;
         document.getElementById("password-needed").hidden = true;
-        emailjs.send("gmail", "template_IN8JsPm4", templateParams, userID);
+        
+        var emailBody = $("form#emailBody");
+        emailBody.submit(function(event){
+            event.preventDefault();
+
+        // Change to your service ID, or keep using the default service
+        var service_id = "default_service";
+        var template_id = "template_IN8JsPm4";
+        emailjs.sendForm(service_id,template_id,emailBody[0])
+            .then(function(){ 
+                alert("Sent!");
+            }, function(err) {
+            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+            });
+        return false;
+        });
     }
     else
     {
