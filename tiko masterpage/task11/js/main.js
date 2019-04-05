70d2b5d9e109160558b2cbfebe622e4612b1cad4
Noah Bauer <147219@msa.asd20.org>
2019-04-05 13:10:40 -0600

    ur gay

--- tiko masterpage/task11/js/main.js
+++ tiko masterpage/task11/js/main.js
@@ -1,4 +1,42 @@
-var album1Pics[] = document.getElementsByClassName("album1-pics");
-function ok(){
-  console.log(album1Pics)
+var album1Pics = document.getElementsByClassName("Album1");
+var album2Pics = document.getElementsByClassName("Album2");
+var album3Pics = document.getElementsByClassName("Album3");
+var album4Pics = document.getElementsByClassName("Album4");
+var arg = 1;
+
+document.getElementsByClassName("Album1").hidden = true;
+function chooseAlbum(arg) {
+  switch (arg) {
+    case 1:
+      for (var i = 0; i < 8; i++) {album1Pics[i].hidden = false;}
+      for (var i = 0; i < 8; i++) {album2Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album3Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album4Pics[i].hidden = true;}
+      break;
+    case 2:
+      for (var i = 0; i < 8; i++) {album1Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album2Pics[i].hidden = false;}
+      for (var i = 0; i < 8; i++) {album3Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album4Pics[i].hidden = true;}
+      break;
+    case 3:
+      for (var i = 0; i < 8; i++) {album1Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album2Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album3Pics[i].hidden = false;}
+      for (var i = 0; i < 8; i++) {album4Pics[i].hidden = true;}
+      break;
+    case 4:
+      for (var i = 0; i < 8; i++) {album1Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album2Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album3Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album4Pics[i].hidden = false;}
+      break;
+    default:
+      for (var i = 0; i < 8; i++) {album1Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album2Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album3Pics[i].hidden = true;}
+      for (var i = 0; i < 8; i++) {album4Pics[i].hidden = true;}
+      break;
+  }
+
 }
