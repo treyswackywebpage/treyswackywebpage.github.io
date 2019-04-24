e896434ee7317bf78bd4c284747ae49ef65c2b51
Trey DeaBueno <146798@msa.asd20.org>
2019-04-24 12:29:32 -0600

    k

--- trey masterpage/Task 11/js/javascript.js
+++ trey masterpage/Task 11/js/javascript.js
@@ -72,7 +72,7 @@ function showSlides(n) {
 
 function setText(n) {
     var text = document.getElementById("slideshow" + albumNumber).getElementsByClassName("albumText");
-    abcdefghij
+    abcdefghijk
     text.value = document.getElementById("slideshow" + albumNumber).getElementsByClassName(slideIndex)[0].src;
 
 }
